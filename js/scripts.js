5fc6a19f91d92aa50e856fe06578e61dc41641ec
UMULISA12 <umulisaa0@gmail.com>
2019-01-14 14:53:17 +0200

    creating first function

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//Back-end
+function Player(name, score, roundScore, totalScore, hold) {
+    this.name;
+    this.score = 0;
+    this.roundScore = 0;
+    this.totalScore = 0;
+    this.image;
+  }
