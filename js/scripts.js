1b480bd490e946fc8ce21d2464ace4b893a027cd
UMULISA12 <umulisaa0@gmail.com>
2019-01-14 14:55:00 +0200

    creating second function

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,6 @@ function Player(name, score, roundScore, totalScore, hold) {
     this.totalScore = 0;
     this.image;
   }
+  function rollDice() {
+    return Math.floor(Math.random() * (7 - 1)) + 1;
+  }
